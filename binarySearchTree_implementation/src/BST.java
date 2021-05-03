0aad2dde84c2617ece1197071b32521a49629178
testForMe-Now <abcodehash@gmail.com>
2021-05-03 13:54:27 +0530

    bst with three nodes is created and added

---
+++ binarySearchTree_implementation/src/BST.java
@@ -0,0 +1,49 @@
+class BST {
+    
+    Node root;
+ 
+   
+    BST()
+    {
+         root = null;
+    }
+ 
+    
+    void insert(int key)
+    {
+         root = insertRec(root, key);
+    }
+ 
+    
+    Node insertRec(Node root, int key)
+    {
+ 
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+ 
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+ 
+        return root;
+    }
+ 
+    void show()
+    {
+         inorderRec(root);
+    }
+ 
+    void inorderRec(Node root)
+    {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+    
+}
---
+++ binarySearchTree_implementation/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        BST tree = new BST();
+ 
+        
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+        
+        tree.show();
+       
+    }
+}
